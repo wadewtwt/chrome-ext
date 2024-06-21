13c5e5484331ca1ee6f316ed4e511fd703a4e624
wt <1078210460@qq.com>
2024-06-21 14:55:25 +0800

    logo好了

--- scripts/test.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.sendMessage({ type: "checkFlag" }, (response) => {
-    if (response && response.hasOwnProperty("runtime")) {
-        if (response.runtime) {
-
-            document.getElementById("su").style.color="red";
-
-        }
-    }
-});
